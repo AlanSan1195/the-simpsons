3eff7f965aced02bf221ac2fa5a7565bc196233d
Alan San <alansandovalt1@gmail.com>
2025-12-04 18:14:23 -0600

    perfeccionar estructura

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -1,13 +1,22 @@
 const url = "https://thesimpsonsapi.com/api/characters"
-async function fetchSimpsons() {
+
+
+interface Characters {
+    id: number;
+    name: String;
+    portrait_path: String;
+    age?: number;
+    phrases?: String[];
+}
+export async function fetchSimpsons(): Promise<Characters[]> {
     try {
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data)
         return data.results || [];
     } catch(error) {
         console.error("Error:", error);
-        return [];
+        return [] as Characters[];
     }
 }
 
-export default fetchSimpsons;
